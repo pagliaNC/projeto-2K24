09862ac50b9ccb9ff2aac619062a2fc74c493f57
pagliaNC <105728352+pagliaNC@users.noreply.github.com>
2024-04-17 13:46:55 -0300

    Update funcoes_javascript.js

--- funcoes_javascript.js
+++ funcoes_javascript.js
@@ -2,12 +2,12 @@ function somar(num1, num2){
   return num1+num2;
 }
 function diminuiir(num1, num2){
-  return num1+num2;
+  return num1-num2;
 }
 function multiplicar(num1, num2){
-  return num1+num2;
+  return num1*num2;
 }
 function dividir(num1, num2){
-  return num1+num2;
+  return num1/num2;
 }
 
